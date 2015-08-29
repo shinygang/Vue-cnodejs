a501bf1c30d3d36d9792a71900511d701eec05d0
唐岗 <429517385@qq.com>
2015-08-29 21:05:27 +0800

    处理列表

---
+++ js/main.js
@@ -0,0 +1,8 @@
+require.config({
+    baseUrl: '/bower_components',
+    paths: {
+        jquery: 'jquery/dist/jquery.min',
+        materialize: 'materialize/dist/js/materialize.min',
+        vue: 'vue/dist/vue.min'
+    }
+});
